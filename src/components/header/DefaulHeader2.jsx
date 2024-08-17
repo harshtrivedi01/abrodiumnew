050d67532987cc4c4c64bfc80a676995e709fe57
fedev2024 <fe.dev.ba2024@gmail.com>
2024-08-17 21:22:55 +0530

    page-ui

--- src/components/header/DefaulHeader2.jsx
+++ src/components/header/DefaulHeader2.jsx
@@ -1,10 +1,8 @@
-import logo from "../../Images/logo.png"
-
+import logo from "../../Images/logo.png";
 
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import HeaderNavContent from "./HeaderNavContent";
-
 
 const DefaulHeader2 = () => {
   const [navbar, setNavbar] = useState(false);
@@ -23,23 +21,19 @@ const DefaulHeader2 = () => {
 
   return (
     // <!-- Main Header-->
-    <header 
-      className={`main-header font-bold ${
-        navbar ? "fixed-header animated slideInDown " : ""
+    <header
+      className={`main-header font-bold border ${
+        navbar ? "fixed-header animated slideInDown   " : ""
       }`}
     >
       {/* <!-- Main box --> */}
-      <div className="main-box">
+      <div className="main-box ">
         {/* <!--Nav Outer --> */}
         <div className="nav-outer">
           <div className="logo-box">
             <div className="me-10">
               <Link to="/">
-              <img
-                                        alt="brand"
-                                        src={logo}
-                                       className="h-28 w-28"
-                                    />
+                <img alt="brand" src={logo} className="h-20 w-28 " />
               </Link>
             </div>
           </div>
@@ -52,14 +46,17 @@ const DefaulHeader2 = () => {
 
         <div className="outer-box">
           {/* <!-- Add Listing --> */}
-          <Link to="/candidates-dashboard/cv-manager" className="upload-cv text-blue-950">
+          <Link
+            to="/candidates-dashboard/cv-manager"
+            className="upload-cv text-blue-950"
+          >
             Upload your CV
           </Link>
           {/* <!-- Login/Register --> */}
           <div className="btn-box">
             <a
               href="#"
-              className="theme-btn btn-style-three call-modal p-3   text-blue-950"
+              className="theme-btn btn-style-three call-modal p-2   text-blue-950"
               data-bs-toggle="modal"
               data-bs-target="#loginPopupModal"
             >
---
+++ src/components/sentry-spot/hero.jsx
@@ -0,0 +1,46 @@
+const HeroSection = () => {
+  return (
+    <section className="call-to-action ">
+      <div className="auto-container">
+        <div className="flex gap-8 md:gap-16 bg-[#051A49] p-10 rounded-xl ">
+          <div className="flex items-center basis-full md:basis-1/2 py-2 md:py-4">
+            <div className="sec-title ">
+              <h3 className="text-white  text-2xl font-bold">
+                Enhance Your Career Prospects with a SentrySpot ID Profile
+              </h3>
+              <div className="text text-white ">
+                With a SentrySpot ID profile, you'll enjoy a customized job
+                search experience and receive job recommendations that match
+                your skills, making your job hunt quicker and more efficient.
+              </div>
+              <a
+                href="#"
+                className="theme-btn btn-style-one bg-[#E60278]  hover:bg-[#E60278]"
+              >
+                <span className="btn-title">Get Your SentrySpot ID now</span>
+              </a>
+            </div>
+          </div>
+          {/* End .content-column */}
+
+          <div className=" hidden md:flex basis-1/2 bg-contain bg-no-repeat">
+            <img
+              src="/images/resource/sentry_spot_Profile.jpg"
+              alt="resource"
+              className="object-contain "
+            />
+            {/* <figure className="image">
+              <img
+                src="/images/resource/sentry_spot_Profile.jpg"
+                alt="resource"
+              />
+            </figure> */}
+          </div>
+          {/* End .image-column */}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
